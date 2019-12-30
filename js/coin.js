b652fa6ec921781c9218796100f36f007337ca17
Hazel Paiz <paiz.hazel@gmail.com>
2019-12-30 10:57:32 -0800

    added feature to change currency type and 60 second interval for price update

--- js/coin.js
+++ js/coin.js
@@ -1,5 +1,6 @@
 const url = "https://api.coindesk.com/v1/bpi/currentprice.json";
 const priceTag = document.querySelector("h1");
+const paragraphTag= document.querySelector("p");
 let currency = "USD";
 
 
@@ -7,9 +8,33 @@ const checkPrice = function() {
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            priceTag.innerHTML = data.bpi.USD.rate_float.toFixed(1);
+            priceTag.innerHTML = data.bpi[currency].rate_float.toFixed(1)
         })
     }
 
 // run on load
 checkPrice();
+
+// loop over every nav link and add a click event
+const navLinks = document.querySelectorAll('nav a') 
+navLinks.forEach(link => {
+    link.addEventListener('click', function () {
+        currency = this.getAttribute("data-currency")
+        checkPrice()
+
+        // remove all previous selected states
+        navLinks.forEach(link => link.classList.remove("selected"))
+
+        // and THEN only do it on the clicked link
+        this.classList.add("selected");
+        
+        // update the span tag accordingly
+        paragraphTag.innerHTML = currency + " per BTC"
+        
+    })
+})
+
+// check the price every 60 seconds
+setInterval(function () {
+    checkPrice()
+}, 60000)
